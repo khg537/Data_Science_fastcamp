07fa200d48704d43f50bbd26b163e0f1b038550a
khg537 <khg537@gmail.com>
2022-11-26 01:10:17 +0900

    Update log_merge.py

--- log_merge.py
+++ log_merge.py
@@ -3,7 +3,7 @@ import sys
 import pandas as pd
 import datetime
 
-dir_path = "./"
+dir_path = "./log"
 out_file = "./out.txt"
 
 def validate_date(date_text):
@@ -20,6 +20,7 @@ def change_df(file_path):
         lines = rfile.readlines()
         for line in lines:
             line_split = line.split(' ',1)
+            # print(line_split)
             if validate_date(line_split[0]):
                 out_lists.append(line_split)
 
@@ -29,16 +30,25 @@ def change_df(file_path):
     df = pd.DataFrame(out_lists)
     return df
 
-
+list_df = []
 for (root, directories, files) in os.walk(dir_path):
-    empty_df = pd.DataFrame()
-
     for file in files:
         file_path = os.path.join(root, file)
         if os.path.getsize(file_path) > 0:
             df = change_df(file_path)
             print(df.size)
             if df.size >0:
-                empty_df = empty_df.append(df)
+                list_df.append(df)
 
-    # df.to_csv(empty_df)
+
+list_df = pd.concat(list_df)
+
+list_df=list_df.apply(lambda x:x.str.strip(), axis=1)
+
+list_df.columns = ['time', 'value']
+print(list_df.head())
+list_df['time'] = pd.to_datetime(list_df['time'])
+list_df = list_df.sort_values(by='time')
+list_df['time'] = list_df['time'].dt.time
+list_df.info()
+list_df.to_csv('out.txt', sep='\t', index=False)
